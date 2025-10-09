85af198b9dbccf6cf94b901a48ce22c97f050019
twannylvsmusic-maker <twannylvsmusic@gmail.com>
2025-10-09 13:01:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -235,5 +235,6 @@ def clear_all_custom_shapes():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    if __name__ == '__main__':
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port, debug=False)
